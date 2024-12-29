2249d80abaa6ff6c4694df7ee4cac126d34702b0
Elie MEN <133395047+Elimen210@users.noreply.github.com>
2024-12-29 22:53:50 +0100

    Added a social page and posts system

--- Guetto/social/views.py
+++ Guetto/social/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+class PostListView(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all().order_by('-created_on')
+        form = PostForm()
+
+        context = {
+                'post_list' : posts,
+                'form' : form,
+            }
+
+        return render(request, 'social/post_list.html', context)
