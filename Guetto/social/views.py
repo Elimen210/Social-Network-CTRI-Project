597d46e24c06b3abdfe0a4a2be4610808d44b61f
Elie MEN <133395047+Elimen210@users.noreply.github.com>
2024-12-31 19:48:29 +0100

    added comment session for every post of the website
    last commit of 2024 :)

--- Guetto/social/views.py
+++ Guetto/social/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
@@ -9,8 +10,57 @@ class PostListView(View):
         form = PostForm()
 
         context = {
-                'post_list' : posts,
-                'form' : form,
-            }
-
+            'post_list': posts,
+            'form': form,
+        }
         return render(request, 'social/post_list.html', context)
+
+    def post(self, request, *args, **kwargs):
+        post = Post.objects.all().order_by('-created_on')
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+
+        context = {
+            'post_list': post,
+            'form': form,
+        }
+        return render(request, 'social/post_list.html', context)
+
+class PostDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm()
+
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
+
+        return render(request, 'social/post_detail.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+        
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
+
+        return render(request, 'social/post_detail.html', context)
